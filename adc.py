0dd199a1331cceaa9a96e82de50c87daa91f8e23
nmehlman <nmehlman@usc.edu>
2021-01-07 08:18:59 -0500

    adc code

--- adc.py
+++ adc.py
@@ -6,11 +6,11 @@ def adcRead(slave_addr, i2c_addr):
     addr_data_int = int(addr_data, 2)
     control_data = '0b00000100'
     control_data_int = int(control_data, 2)
-    with SMBus(0) as bus:    
+    with SMBus(1) as bus:    
         bus.write_byte_data(i2c_addr, 0, addr_data_int)
         bus.write_byte_data(i2c_addr, 0, control_data_int)
-    with SMBus(0) as bus: 
-        data = bus.read_bus_data(i2c_addr, 0)
+    with SMBus(1) as bus: 
+        pass#data = bus.read_bus_data(i2c_addr, 0)
     return data
 
 while True:
