2bd17e647980ee9d0262036c8e14bff02ae9b396
nmehlman <nmehlman@usc.edu>
2021-01-08 17:39:39 -0500

    ADC

--- adc.py
+++ adc.py
@@ -8,30 +8,18 @@ def adcReadInit(slave_addr : str, i2c_addr: int, adc_ch: int):
     \n12c_addr -> hex adress of i2c connection (i.e. 0x12)
     \nadc_ch -> channel where adc is connected (0-3)'''
 
-    addr_data = '0b' + '1001' + slave_addr + '1'
-    addr_data_int = int(addr_data, 2)
-    
-    control_data = '0b000000' + bin(adc_ch).replace('0b', '') #Assumes non-differential input
-    control_data_int = int(control_data, 2)
-    
-    i2c_addr = int(str(i2c_addr), 16)
+    address = 0x48
+    cmd = 0x40
+    value = 0
 
     bus = SMBus(1)
     while True:
-        bus.write_byte(i2c_addr,addr_data_int) #Adress
-        sample = bus.read_byte(i2c_addr)
-        print(sample)
-
-    return sample
-
-def getSample(i2c_addr: int):
-
-    i2c_addr = int(str(i2c_addr), 16)
-
-    with SMBus(1) as bus:    
-        sample = bus.read_byte(i2c_addr, 0)
-
-    return sample
+        bus.write_byte_data(address,cmd,value)
+        value += 1
+        if value == 256:
+            value =0
+        print("AOUT:%3d" %value)
+        sleep(0.01)
 
 adcReadInit('000', 48, 0)
 while True:
