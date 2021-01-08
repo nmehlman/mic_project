8f0cfb24ab01db0e70e18c6e8a3b67d7a938b94c
nmehlman <nmehlman@usc.edu>
2021-01-08 17:53:33 -0500

    ADC

--- adc.py
+++ adc.py
@@ -1,25 +1,26 @@
 from smbus2 import SMBus
 from time import sleep
 
-def adcReadInit(slave_addr : str, i2c_addr: int, adc_ch: int):
+def adcReadInit(bus, i2c_addr, slave_addr, adc_ch):
 
     '''Reads data from ADC.
+    \nbus -> SMBus object
     \nslave_addr -> configuration of A0-A2 in string form (i.e. '010')
     \n12c_addr -> hex adress of i2c connection (i.e. 0x12)
     \nadc_ch -> channel where adc is connected (0-3)'''
 
-    address = 0x48
-    A0 = 0x40
-    A1 = 0x41
-    A2 = 0x42
-    A3 = 0x43
-    bus = SMBus(1)
-    bus.write_byte(address,A0)
-    
-    while True:
-        value = bus.read_byte(address)
-        print("AOUT:%1.3f  " %(value*3.3/255))
-        sleep(0.1)
-   
+    addr_byte = '0b1001' + slave_addr + '1'
+    control_byte = hex(adc_ch)
 
-adcReadInit(1,1,1)
+    bus.write_byte(i2c_addr, addr_byte)
+    bus.write_byte(i2c_addr, addr_byte)
+    
+def getSample(bus, i2c_addr):
+    return bus.read_byte(i2c_addr)
+    
+
+bus = SMBus(1)
+adcReadInit(bus, '000', 0X48, 0)
+while True:
+    print(getSample(bus, i2c_addr))
+    sleep(.1)
