9c034da04fb0f4545ab39030cdb513b9cd1fd984
nmehlman <nmehlman@usc.edu>
2021-01-10 13:48:49 -0500

    Code for USB mic

--- audio_IO.py
+++ audio_IO.py
@@ -20,7 +20,7 @@ frames = []
 
 # loop through stream and append audio chunks to frame array
 for ii in range(0,int((sr/chunk)*rec_len)):
-    data = stream.read(chunk)
+    data = stream.read(chunk, exception_on_overflow=False)
     frames.append(data)
 
 
